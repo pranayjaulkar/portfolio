2c6001f0bc830c6757177ade0a976c1a1244def7
pranay-jaulkar <pranaypj13@gmail.com>
2025-09-08 10:19:39 +0530

    fixed minor build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        path.join(process.cwd(), ".git/**"),
+        path.join(process.cwd(), ".next/**"),
+        path.join(process.cwd(), "node_modules/**"),
+        path.join(process.cwd(), "public/static/**"),
+      ],
+    },
   },
 };
 
