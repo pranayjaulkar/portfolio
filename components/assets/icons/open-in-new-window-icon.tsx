1dc71bd6dda356a252b923d5c60f30301ee133eb
pranay-jaulkar <pranaypj13@gmail.com>
2025-09-08 10:10:07 +0530

    fixed build errors

--- components/assets/icons/open-in-new-window-icon.tsx
+++ components/assets/icons/open-in-new-window-icon.tsx
@@ -28,7 +28,6 @@ export default function GithubLightIcon({
         fontFamily="none"
         fontWeight="none"
         fontSize="none"
-        textAnchor="none"
       >
         <g transform="scale(10.66667,10.66667)">
           <path d="M5,3c-1.09306,0 -2,0.90694 -2,2v14c0,1.09306 0.90694,2 2,2h14c1.09306,0 2,-0.90694 2,-2v-7h-2v7h-14v-14h7v-2zM14,3v2h3.58594l-9.29297,9.29297l1.41406,1.41406l9.29297,-9.29297v3.58594h2v-7z"></path>
